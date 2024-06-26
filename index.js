549d84b9b4819a4da20f20d2d412845d47476a9c
Vaibhav Kumar <vaibhav14.vk@gmail.com>
2024-06-26 13:39:45 +0530

    first Api struct

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const transferRouter = require('./src/routes/transfer');
+
+const app = express();
+app.use('/api', transferRouter);
+
+const PORT = 3000;
+
+
+function onStart(){
+    console.log(`Server running on port ${PORT}`);
+}
+
+app.listen(PORT, onStart);
+
+module.exports = app;
